dfb50a8b45a8ab5c40be5d409fea07a15ca2dac4
McJeffr <mcjeffr@gmail.com>
2022-12-01 21:13:55 +0100

    Day 1.

---
+++ src/main/kotlin/com/mcjeffr/aoc/day1/Day1.kt
@@ -0,0 +1,24 @@
+package com.mcjeffr.aoc.day1
+
+import com.mcjeffr.aoc.common.PuzzleInput
+
+fun main() {
+    val input = PuzzleInput.getInput(1)
+
+    var elf = 0
+    val elves: List<Int> = input
+        .groupBy { line ->
+            if (line.isBlank()) {
+                elf++
+                return@groupBy -1
+            }
+            elf
+        }
+        .filterKeys { it != -1 }
+        .mapValues { entry -> entry.value.sumOf { it.toInt() } }
+        .values
+        .sortedDescending()
+
+    println("Top elf carries: ${elves.max()}")
+    println("Top three elves carry a total of: ${elves.take(3).sum()}")
+}
